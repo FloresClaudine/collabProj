5da41613709aa39f58efc1d5b6ad9e258e1985be
FloresClaudine <150830537+FloresClaudine@users.noreply.github.com>
2024-02-11 15:38:08 +0800

    Add files via upload

---
+++ CollectiveCrafters.js
@@ -0,0 +1,58 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 80);
+});
+
+
+
+
+function scrollToShop() {
+    var shopSection = document.getElementById("shop");
+    var offsetTop = shopSection.offsetTop;
+    window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+}
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick =() =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+
+};
+
+window.onscroll  =() =>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+
+};
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration: '2500',
+    reset: true
+});
+
+sr.reveal('.home-text', { delay: 300 }); 
+sr.reveal('.home-logo', { delay: 400 }); 
+sr.reveal('.container', { delay: 400 }); 
+
+sr.reveal('.about-img', {}); 
+sr.reveal('.about-text', { delay: 300 }); 
+
+sr.reveal('.middle-text', {}); 
+sr.reveal('.row-btn,.shop-content', { delay: 300 }); 
+
+sr.reveal('.review-content,.contact', { delay: 300 }); 
+
+
+
+
+
+
+
